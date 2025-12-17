0b245122f7d6a2247d1285efbd55d1ff8f9a27a5
GuillermoFigueroa <figueroalopezguillermo@gmail.com>
2025-12-17 11:55:12 -0300

    fix: ajustar ancho del modal a 880px

--- build/assets/authService-BNGNkTaa.js
+++ build/assets/authService-BNGNkTaa.js
@@ -1,4 +1,4 @@
-import{b as o,s}from"./supabase-Cl4G1Yfh.js";/**
+import{b as o,s}from"./supabase-BoQfidNv.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.

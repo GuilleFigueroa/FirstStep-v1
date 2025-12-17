d016c891a31d5750cf8f8c940bba31ff7ead0ee6
GuillermoFigueroa <figueroalopezguillermo@gmail.com>
2025-12-17 11:52:53 -0300

    revert: quitar cambios de flex, solo mantener ancho de 1100px

--- build/assets/authService-DQn-UMDx.js
+++ build/assets/authService-DQn-UMDx.js
@@ -1,4 +1,4 @@
-import{b as o,s}from"./supabase-DKX0dIAf.js";/**
+import{b as o,s}from"./supabase-Cl4G1Yfh.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
